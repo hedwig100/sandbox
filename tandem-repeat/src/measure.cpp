15a51b02252efff2e3ff294f71162fab3001d20a
hedwig100 <shota.in26@gmail.com>
2023-06-29 20:44:39 +0900

    [fix] measure

--- tandem-repeat/src/measure.cpp
+++ tandem-repeat/src/measure.cpp
@@ -9,13 +9,15 @@ double Measure(std::function<void(std::string)> f, const std::string &seq) {
 }
 
 int main() {
+    const int n = 10000;
     RandomGenerator<std::string> rg;
-    std::string seq = rg.generate(3000);
+    std::string seq = rg.generate(n);
 
     auto t0 = Measure([](std::string s) { removeNonMaximal(Naive(s)); }, seq);
     auto t1 = Measure([](std::string s) { removeNonMaximal(DivideConquer(s)); }, seq);
     auto t2 = Measure([](std::string s) { removeNonMaximal(DivideConquerFast(s)); }, seq);
 
+    std::cout << "n: " << n << '\n';
     std::cout << "Naive:" << t0 << "[ms], DivideConquer: " << t1 << "[ms], DivideConquerFast: " << t2 << "[ms] \n";
 
     return 0;
