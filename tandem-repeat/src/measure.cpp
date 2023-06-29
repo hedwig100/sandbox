778524eefdceeabd6036825b9976929cd1ae22a7
hedwig100 <shota.in26@gmail.com>
2023-06-29 11:38:26 +0900

    [add] measure

--- tandem-repeat/src/measure.cpp
+++ tandem-repeat/src/measure.cpp
@@ -1,1 +1,22 @@
+#include "algo.hpp"
 
+double Measure(std::function<void(std::string)> f, const std::string &seq) {
+    clock_t start, end;
+    start = clock();
+    f(seq);
+    end = clock();
+    return (end - start) / CLOCKS_PER_SEC;
+}
+
+int main() {
+    RandomGenerator<std::string> rg;
+    std::string seq = rg.generate(3000);
+
+    auto t0 = Measure([](std::string s) { removeNonMaximal(Naive(s)); }, seq);
+    auto t1 = Measure([](std::string s) { removeNonMaximal(DivideConquer(s)); }, seq);
+    auto t2 = Measure([](std::string s) { removeNonMaximal(DivideConquerFast(s)); }, seq);
+
+    std::cout << "Naive:" << t0 << "s, DivideConquer: " << t1 << "s, DivideConquerFast: " << t2 << "s \n";
+
+    return 0;
+}
