aac0733f00bf6cb1376cc4410984add31d2b36be
hedwig100 <shota.in26@gmail.com>
2024-06-19 22:22:27 +0900

    [add] BNode functions

--- build-your-own-db-go/main.go
+++ build-your-own-db-go/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/binary"
+	"log"
+	"os"
+)
+
 const HEADER = 4
 
 const BTREE_PAGE_SIZE = 4096
@@ -8,5 +14,55 @@ const BTREE_MAZ_VAL_SIZE = 3000
 
 func init() {
 	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAZ_VAL_SIZE
-	_ = node1max
+	if node1max > BTREE_PAGE_SIZE {
+		log.Fatal("max node size execeeds max page size")
+	}
+}
+
+type BNode []byte
+
+const (
+	BNODE_NODE = 1
+	BNODE_LEAF = 2
+)
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	if idx >= node.nkeys() {
+		os.Exit(1)
+	}
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+	pos := HEADER + 8*idx
+	binary.LittleEndian.PutUint64(node[pos:], val)
+}
+
+type BTree struct {
+	// pointer (a nonzero page number)
+	root uint64
+
+	// callbacks for managing on-disk pages
+	// get dereference a pointer
+	get func(uint64) []byte
+
+	// new allocate a new page
+	new func([]byte) uint64
+
+	// del deallocate a page
+	del func(uint64)
 }
