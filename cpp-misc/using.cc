011566bfe0423c6ae3358b7d81397f8d93efa1e4
hedwig100 <shota.in26@gmail.com>
2025-03-30 17:04:01 +0900

    [add] using.cc

--- cpp-misc/using.cc
+++ cpp-misc/using.cc
@@ -20,6 +20,24 @@
 // }
 
 /**
+ * 結論としては再帰的な型をusingを使って定義することは出来なそう。
+ * ポインタ型にしても、再帰的に利用したい型をusingで定義していて宣言ができないのでできない。
+ * なのでやるならstd::variantを持つ構造体とかを使うのがいい。
+ */
+
+// using Value2 = int;
+// using Mul2 = std::tuple<Expr2*, Expr2*>;
+// using Add2 = std::tuple<Mul2*, Mul2*>;
+// using Expr2 = std::variant<Value2*, Add2*, Mul2*>;
+
+// int ptr_using() {
+//     // Create an expression tree for the expression (1 + 2) + (3 + 4)
+//     Value2 value = 1;
+//     Expr2 expr = &value;
+//     return 0;
+// }
+
+/**
  * 以下のコードはコンパイルできる。
  * 再帰的な型を定義するときはポインタを使う必要あり(型のサイズが計算できなくなるので)
  */
@@ -49,7 +67,7 @@ struct Expr {
     std::variant<Value*, Add*, Mul*> value;
 };
 
-int main() {
+int ptr_struct() {
     // Create an expression tree for the expression (1 + 2) + (3 + 4)
     Value value = 1;
     Expr expr = {&value};
@@ -59,5 +77,11 @@ int main() {
     Add add = {&mul, &mul};
 
     std::cout << "Expression tree created successfully." << std::endl;
+    return 0;
+}
+
+int main() {
+    // ptr_using();
+    ptr_struct();
     return 0;
 }
