a8aafdf3f2b34d58d3d9c85304019c6714962ae6
hedwig100 <shota.in26@gmail.com>
2025-03-30 12:26:55 +0900

    [add] sample for recursive type

--- cpp-misc/using.cc
+++ cpp-misc/using.cc
@@ -2,19 +2,62 @@
 #include <variant>
 #include <iostream>
 
-using Value = int;
-using Mul = std::tuple<Expr, Expr>;
-using Add = std::tuple<Mul, Mul>;
-using Expr = std::variant<Value, Add, Mul>;
-
 /**
  * 結論としては再帰的な型をusingを使って定義することは出来なさそう
  * 型推論に失敗していそう。このコードはコンパイルできない。
  * なのでやるならstd::variantを持つ構造体とかを使うのがいい。
  */
 
+// using Value = int;
+// using Mul = std::tuple<Expr, Expr>;
+// using Add = std::tuple<Mul, Mul>;
+// using Expr = std::variant<Value, Add, Mul>;
+
+// int main() {
+//     // Create an expression tree for the expression (1 + 2) + (3 + 4)
+//     Value value = 1;
+//     Expr expr = value;
+// }
+
+/**
+ * 以下のコードはコンパイルできる。
+ * 再帰的な型を定義するときはポインタを使う必要あり(型のサイズが計算できなくなるので)
+ */
+
+
+using Value = int;
+
+struct Expr;
+
+struct Mul {
+    Mul() = default;
+    Mul(Expr *left, Expr *right) : values(left, right) {}
+    std::tuple<Expr*, Expr*> values;
+};
+
+struct Add {
+    Add() = default;
+    Add(Mul *left, Mul *right) : values(left, right) {}
+    std::tuple<Mul*, Mul*> values;
+};
+
+struct Expr {
+    Expr() = default;
+    Expr(Value *value) : value(value) {}
+    Expr(Mul *mul) : value(mul) {}
+    Expr(Add *add) : value(add) {}
+    std::variant<Value*, Add*, Mul*> value;
+};
+
 int main() {
     // Create an expression tree for the expression (1 + 2) + (3 + 4)
     Value value = 1;
-    Expr expr = value;
+    Expr expr = {&value};
+
+    // You can also create Add and Mul expressions
+    Mul mul = {&expr, &expr};
+    Add add = {&mul, &mul};
+
+    std::cout << "Expression tree created successfully." << std::endl;
+    return 0;
 }
