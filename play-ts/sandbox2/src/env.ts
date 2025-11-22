4b5d0ed9525d2f62b5df42cbd403990a0237fecd
hedwig100 <shota.in26@gmail.com>
2025-11-22 17:07:21 +0900

    add: zod

---
+++ play-ts/sandbox2/src/env.ts
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const EnvSchema = z.object({
+  PORT: z.string().transform(Number),
+  DATABASE_URL: z.string().url(),
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+});
+
+export const env = EnvSchema.parse(process.env);
--- play-ts/sandbox2/src/main.ts
+++ play-ts/sandbox2/src/main.ts
@@ -1,13 +1,14 @@
 import { exportAdd } from "@/lib.js";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { env } from "@/env";
 
 const main = (): void => {
     console.log("This is sandbox2 main.ts");
     const result = exportAdd(5, 7);
     console.log(`5 + 7 = ${result}`);
-    console.log(`Your secret from .env is: ${process.env.YOUR_SECRET}`);
+    console.log(`Your database url from .env is: ${env.DATABASE_URL}`);
+    console.log(`Your port from .env is: ${env.PORT}`);
+    console.log(`Your node_env from .env is: ${env.NODE_ENV}`);
+
 };
 
 main();
