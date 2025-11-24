fd1a8e2744301ad583dc39c23ef58a78cd069163
hedwig100 <shota.in26@gmail.com>
2025-11-24 16:55:34 +0900

    add: express server with typescript

--- play-ts/sandbox3/src/index.ts
+++ play-ts/sandbox3/src/index.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { type Chord, type BaseSound, ChordSchema } from './types/chord.js';
+import { validateMiddleware } from './middleware/validate.js';
+import { loggerMiddleware } from './middleware/logging.js';
+import { ChordTones } from './services/chord.js';
 
 const app: express.Application = express();
 const PORT: number = 3000;
 
+app.use(express.json());
+app.use(loggerMiddleware);
+
 app.get('/health', (req: express.Request, res: express.Response) => {
   res.status(200).send('OK');
+});
+
+app.post('/chord', validateMiddleware(ChordSchema), (req: express.Request, res: express.Response) => {
+  const chord: Chord = req.body;
+  const tones: BaseSound[] = ChordTones(chord);
+  res.status(200).json(tones);
 });
 
 app.listen(PORT, () => {
